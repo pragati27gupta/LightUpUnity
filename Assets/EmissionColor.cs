90ec4f6aa42250db7ddafb09cf393ea296d45710
Pragati Gupta <pragati27gupta@gmail.com>
2019-10-15 11:41:33 -0700

    CHLA Unity Light Up Scene

---
+++ Assets/EmissionColor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class EmissionColor : MonoBehaviour
+{
+    int count = 0;
+    Material mat;
+    GameObject go;
+    Light lt;
+    private void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        go = GetComponent<Renderer>().gameObject;
+        lt = GetComponent<Light>();
+        mat.EnableKeyword("_EMISSION");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("a")) {
+            if (count < 5)
+            {
+                count++;
+                Debug.Log(count);
+                for (int i = 0; i < 10; i++)
+                {
+                    go.transform.localScale += new Vector3(0.05f, 0.05f, 0.05f);
+                    lt.intensity += 0.005f;
+                }
+                //mat.SetColor("_EmissionColor", Color.white);
+            }
+        }
+    }
+}
